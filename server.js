02884581d34be187d7c1e8713452b12902f277a0
a7medadel309 <60558496+a7medadel309@users.noreply.github.com>
2026-08-29 23:25:20 +0300

    fix: configure express api for vercel

--- server.js
+++ server.js
@@ -3,20 +3,12 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT =
-    process.env.PORT || 3000;
+connectDB()
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
 
-const startServer = async () => {
-    await connectDB();
-
-    // app.listen(
-    //     PORT,
-    //     () => {
-    //         console.log(
-    //             `Server running on http://localhost:${PORT}`
-    //         );
-    //     }
-    // );
-};
-
-startServer();
+module.exports = app;
